3b545b0f0af71303a3f106fac1d4f31575aa39b1
Willians Mansueto <31316180+WillMansueto@users.noreply.github.com>
2023-06-05 00:10:49 -0300

    Create Populate DB.sql

---
+++ Final Paper Library/Populate DB.sql
@@ -0,0 +1,65 @@
+-- Inserção na tabela Curso
+INSERT INTO Curso (id_curso, nome_curso) VALUES (1, 'Sistemas Biomédicos');
+
+INSERT INTO Curso (id_curso, nome_curso) VALUES (2, 'Automação Industrial');
+
+INSERT INTO Curso (id_curso, nome_curso) VALUES (3, 'Banco de Dados');
+
+INSERT INTO Curso (id_curso, nome_curso) VALUES (4, 'Redes de Computadores');
+
+-- Inserção na tabela Aluno
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (1,  'João da Silva',    '119490', '2020/1', '2023/2', 1);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (2,  'Maria Santos',     '812760', '2020/1', '2023/2', 1);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (3,  'Carla Ferreira',   '123456', '2020/1', '2023/2', 1);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (4,  'João Santos',      '234567', '2020/1', '2023/2', 1);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (5,  'Gabriel Pereira',  '929963', '2020/1', '2023/2', 2);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (6,  'Ana Silva',        '201458', '2020/1', '2023/2', 2);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (7,  'Pedro Almeida',    '196362', '2020/1', '2023/2', 2);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (8,  'Maria Oliveira',   '966462', '2020/1', '2023/2', 2);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (9,  'Bianca Castro',    '804711', '2020/1', '2023/2', 3);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (10, 'Juliana Costa',    '169136', '2020/1', '2023/2', 3);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (11, 'Gustavo Oliveira', '170342', '2020/1', '2023/2', 3);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (12, 'Bruno Oliveira',   '779036', '2020/1', '2023/2', 3);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (13, 'Mariana Lima',     '566235', '2020/1', '2023/2', 4);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (14, 'Thiago Silva',     '359309', '2020/1', '2023/2', 4);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (15, 'Henrique Alves',   '443974', '2020/1', '2023/2', 4);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (16, 'Giovanna Ribeiro', '924492', '2020/1', '2023/2', 4);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (17, 'Marcelo Cardoso',  '798802', '2020/1', '2023/2', 3);
+
+INSERT INTO Aluno (id_aluno, nome, registro_academico, ano_semestre_ingresso, ano_semestre_conclusao, id_curso)
+VALUES (18, 'Daniel Souza',     '284822', '2020/1', '2023/2', 3);
+
